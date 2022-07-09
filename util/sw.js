15c43f409e217a7402f333828be476c8b3fbf34c
itIsBrando <mooballygoobally@gmail.com>
2022-07-09 12:27:33 -0400

    Fixed Service worker not getting installed

--- util/sw.js
+++ util/sw.js
@@ -1,62 +1,53 @@
+const log = function(t) {
+    console.log(`[Service Worker] ${t}`);
+}
 
 const CACHE_NAME = 'gbemu-v3.8.1';
 const FILES = [
-    "util/",
-    "icons/",
-    "css/",
-    "javascript/",
-    "css/style.css",
-    "css/touch.css",
-    "css/debug.css",
-    "css/buttons.css",
-    "css/themes.css",
-    "javascript/mbc",
-    "javascript/audio",
-    "util/saveManager.js",
-    "util/promptMenu.js",
-    "util/utility.js",
-    "util/settings.js",
-    "util/controller.js",
-    "util/keyboard.js",
-    "util/debug.js",
-    "util/themes.js",
-    "util/cheats.js",
-    "util/touch.js",
-    "util/link.js",
-    "javascript/types.js",
-    "javascript/renderer.js",
-    "javascript/mbc/MBC1.js",
-    "javascript/mbc/MBC2.js",
-    "javascript/mbc/MBC3.js",
-    "javascript/mbc/MBC5.js",
-    "javascript/audio/apu.js",
-    "javascript/audio/c1.js",
-    "javascript/audio/c2.js",
-    "javascript/audio/c3.js",
-    "javascript/audio/c4.js",
-    "javascript/serial.js",
-    "javascript/ppu.js",
-    "javascript/instructionCB.js",
-    "javascript/instruction.js",
-    "javascript/timer.js",
-    "javascript/register.js",
-    "javascript/cpu.js",
-    "javascript/initalize.js",
-    "icons/icon.jpg",
-    "manifest.webmanifest",
-    "index.html",
+    "/css/style.css",
+    "/css/touch.css",
+    "/css/debug.css",
+    "/css/buttons.css",
+    "/css/themes.css",
+    "/javascript/mbc",
+    "/util/saveManager.js",
+    "/util/promptMenu.js",
+    "/util/utility.js",
+    "/util/settings.js",
+    "/util/controller.js",
+    "/util/keyboard.js",
+    "/util/debug.js",
+    "/util/themes.js",
+    "/util/cheats.js",
+    "/util/touch.js",
+    "/util/link.js",
+    "/javascript/types.js",
+    "/javascript/renderer.js",
+    "/javascript/mbc/MBC1.js",
+    "/javascript/mbc/MBC2.js",
+    "/javascript/mbc/MBC3.js",
+    "/javascript/mbc/MBC5.js",
+    "/javascript/serial.js",
+    "/javascript/ppu.js",
+    "/javascript/instructionCB.js",
+    "/javascript/instruction.js",
+    "/javascript/timer.js",
+    "/javascript/register.js",
+    "/javascript/cpu.js",
+    "/javascript/initalize.js",
+    "/index.html",
 ]
 
 /**
  * Fetches files to cache
  */
 self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Installing');
+    log('Installing');
 
     e.waitUntil((async () => {
         const cache = await caches.open(CACHE_NAME);
 
-        console.log('[Service Worker] Caching all: app shell and content');
+        log('Caching all: app shell and content');
         await cache.addAll(FILES);
     })());
 });
@@ -66,6 +57,7 @@ self.addEventListener('install', (e) => {
  * Delete old cache versions
  */
 self.addEventListener('activate', (e) => {
+    log('activated');
     e.waitUntil(caches.keys().then((keyList) => {
         return Promise.all(keyList.map((key) => {
             if(key === CACHE_NAME) {
@@ -76,4 +68,17 @@ self.addEventListener('activate', (e) => {
         }))
     }));
 });
-    
+
+self.addEventListener('fetch', (e) => {
+    log('fetching');
+    e.respondWith((async () => {
+      const r = await caches.match(e.request);
+      log(`Fetching resource: ${e.request.url}`);
+      if (r) return r;
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      log(`Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+  });
